97121b9dea89e07c4da4b7fe58286ba3bea94c2b
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-06-09 20:23:37 +0200

    add-base

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,5 +1,4 @@
 export const selectContacts = (state) => state.contacts.items;
-export const selectNameFilter = (state) => state.filters.status;
 export const selectLoading = (state) => state.contacts.loading;
 export const selectError = (state) => state.contacts.error;
 
